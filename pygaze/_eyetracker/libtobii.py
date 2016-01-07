401e660bd9ec77282442c35009a14fd7ca845615
Björn Guth <bjoern.guth@rwth-aachen.de>
2016-01-07 16:16:32 +0100

    use saccade threshold values from default.py
    fixes #47

--- pygaze/_eyetracker/libtobii.py
+++ pygaze/_eyetracker/libtobii.py
@@ -96,8 +96,8 @@ class TobiiTracker(BaseEyeTracker):
 	"""A class for Tobii EyeTracker objects"""
 	
 	def __init__(self, display, logfile=settings.LOGFILE,
-		eventdetection=settings.EVENTDETECTION, saccade_velocity_threshold=35,
-		saccade_acceleration_threshold=9500, **args):
+		eventdetection=settings.EVENTDETECTION, saccade_velocity_threshold=settings.SACCVELTHRESH,
+		saccade_acceleration_threshold=settings.SACCACCTHRESH, **args):
 		
 		"""Initializes a TobiiTracker instance
 		
